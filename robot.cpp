e99553a724a3d0774e349df213aa74e67de91a1c
Kerim <kerim.grbz.01@gmail.com>
2024-12-15 11:08:30 +0100

    version 1; classe Robot permet de modéliser un robot simple se déplaçant le long d'un axe horizontal avec la possibilité de changer de direction à tout moment.

---
+++ robot.cpp
@@ -0,0 +1,62 @@
+//
+// Created by Krm on 15/12/2024.
+//
+
+/*
+* class Robot{
+
+private:
+int position;
+int direction; // 1 pour droite, -1 pour gauche
+
+public:
+// constructeur avec paramètre, valeur par défaut = 0
+Robot(int pos=0) : position(pos), direction(1){}
+
+//Méthode pour deplacer le robot;
+void deplacer (int n =1){
+    if(n > 0){
+    position += direction * n;
+    }
+    // si n <= 0 , ne rien faire
+}
+
+// Méthode pour faire demi-tour
+void faireDemiTour(){
+    direction *= -1;
+}
+
+// Méthode pour obtenir la position actuelle
+int getPosition() const{
+return position;
+}
+};
+
+int main(){
+Robot r1, r2(10);
+cout<<"Position courante de r2: "<<r2.getPosition()<<endl;
+    cout<<"postion courante de r1 initiale: "<<r1.getPosition()<<endl;
+
+
+    r1.deplacer();
+    cout<<"postion courante de r1: " <<r1.getPosition()<<endl;
+
+    r1.deplacer(-1);
+    cout<<"postion courante de r1: " <<r1.getPosition()<<endl;
+
+    r1.deplacer(2);
+    cout<<"position courante de r1 : "<<r1.getPosition()<<endl;
+
+
+    r1.faireDemiTour();
+    cout<<"position courante de r1 : "<<r1.getPosition()<<endl;
+    r1.deplacer(4);
+    cout << "position courante de r1: " << r1.getPosition() << endl;
+return 0;
+}
+ */
+#include <iostream>
+using namespace std;
+
+
+
