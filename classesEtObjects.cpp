677e9d8a87951c1b04e24f9c31c5ea5ab5575d0c
Kerim <kerim.grbz.01@gmail.com>
2024-12-16 22:07:22 +0100

    variable membre privée pour le rayon et inclura des fonctions membres pour calculer l'aire et la circonférence du cercle.

--- classesEtObjects.cpp
+++ classesEtObjects.cpp
@@ -1,6 +1,8 @@
 //
 // Created by Krm on 16/12/2024.
 //
+
+/*
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -49,3 +51,76 @@ int main() {
     }
     cout<<count;
     }
+* */
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Circle {
+private:
+    double radius; // Variable membre privée pour le rayon du cercle
+
+
+public:
+    //constructeur par défaut
+    Circle() : radius(1.0) { // initialise le rayon à 1.0 si aucun argument n'est fourni
+        cout<<"Constructeur par défaut appéle. Rayon initialisé a" << radius<<"."<<endl;
+    }
+
+    //Constructeur parametre
+    Circle(double r): radius(r) {
+        cout<<"Constructeur paramétré applé. Rayon initialisé à "<<radius<<endl;
+    }
+
+    //Méthode pour définir le rayon(setter)
+    void setRadius(double r) {
+        if(r >= 0) {
+            radius =r;
+            cout<<"Rayon mis a jour à "<<radius<<endl;
+        }else {
+            cout<<"Erreur : Le rayon ne peut pas etre négatif ."<<endl;
+        }
+    }
+
+    //Méthode pour obtenir le rayon(getter)
+    double getRadius() const {
+        return radius;
+    }
+
+    //Méthode pour calculer l'aire du cercle
+    double area()const {
+        return M_PI * pow(radius , 2);
+    }
+
+    // Méthode pour calculer la circonférence du cercle
+    double circumference() const {
+        return 2* M_PI * radius; // 2*pi*r
+        }
+};
+
+
+
+int main(){
+    Circle c1;
+    cout<<"Aire de c1 : "<<c1.area()<<endl;
+    cout<<"Circonférence de c1: "<<c1.circumference()<<endl;
+
+
+    //Modification du rayon de c1
+
+    c1.setRadius(3.5);
+    cout<<"Nouvelle aire de c1 : "<<c1.area()<<endl;
+    cout<<"Nouvelle circonférence de c1 "<< c1.circumference()<<endl;
+
+    cout<<"---------------------------------"<<endl;
+
+    c1.setRadius(-3.0);
+return 0;
+}
+
+
+
+
+
+
+
