fe87b8711d77aa13aa9dba4c13795762a0b5d269
Kerim <kerim.grbz.01@gmail.com>
2024-12-20 11:30:17 +0100

    Modification du vecteur et tableaux  v4

--- algorithm3.cpp
+++ algorithm3.cpp
@@ -15,12 +15,23 @@ void display(iterator first, iterator last) {
     cout<<"]\n";
 }
 
+// no 12
+template <typename T>
+void f(const T tab[]) {
+    vector<T> v (tab.begin(), tab.end());
+    display(v.begin(), v.end());
+}
+
+
 vector<int> v{1,2,3,4,5,6,7,8,9,10};
 int tab[] = {1,2,3,4,5,6,7,8,9,10};
 
 int main() {
     // no 5
-    display(v.begin() + 5, v.begin() + 4);
-
+  //  display(v.begin() + 5, v.begin() + 4);
+    // no 15
+    //display(v.rend(), v.rbegin());
+    display(v.begin(), v.end());
+    display(v.rbegin() + 1, v.rend() - 3);
     return 0;
 }
