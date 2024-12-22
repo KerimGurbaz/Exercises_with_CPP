ded66c1bc895274bbbb3c9756709bd4ced9fdf93
Kerim <kerim.grbz.01@gmail.com>
2024-12-22 14:06:51 +0100

    Copier les éléments de la deuxième plage de données v2

--- concatenation.cpp
+++ concatenation.cpp
@@ -2,7 +2,9 @@
 // Created by Krm on 22/12/2024.
 //
 
-#include <iostream>
+
+/*
+* #include <iostream>
 #include <vector>
 #include <array>
 using namespace std;
@@ -51,3 +53,52 @@ int main() {
 
     return 0;
 }
+ */
+
+#include <iostream>
+#include <vector>
+#include <array>
+using namespace std;
+template<typename T, typename It1, typename It2>
+vector<T> concat_tab(It1 begin1, It1 end1, It2 begin2, It2 end2) {
+    vector<T> result;
+
+    for(It1 it = begin1 ; it != end1 ; ++it) {
+        result.push_back(*it);
+    }
+    for(It2 it = begin2; it != end2 ; ++it) {
+        result.push_back(*it);
+    }
+
+    return result;
+}
+
+int main() {
+    vector<int> vec  = {11,12,13};
+    array<int, 5> arr = {21,22, 23, 24, 25};
+
+    vector<int> res = concat_tab<int>(vec.begin(), vec.end(), arr.begin(), arr.end());
+    cout<<"vector : [";
+    for(int i = 0; i<vec.size() ; ++i) {
+        cout<<vec[i] << (i< vec.size() -1 ? ", " : "");
+    }
+    cout<<"]\n";
+
+    cout<<"array : [";
+    for(int i = 0; i<arr.size() ; ++i) {
+        cout<<arr[i] << (i < arr.size()-1 ? ", " : "");
+    }
+    cout<<"]\n";
+
+    cout<<"resultat : [";
+    for(int i = 0; i<res.size() ; ++i) {
+        cout<<res[i] << (i< res.size() -1? ", " : "");
+    }
+    cout<<"]\n";
+
+
+
+}
+
+
+
