282829f9570b3e713e775b355a636f0c0e036e46
Kerim <kerim.grbz.01@gmail.com>
2024-12-27 22:06:17 +0100

    Utilisation des Templates avec swap v2

--- permutationCirculaire.cpp
+++ permutationCirculaire.cpp
@@ -2,6 +2,50 @@
 // Created by Krm on 27/12/2024.
 //
 #include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+template <typename T>
+void permutationCirculaireDroite(T& a, T& b, T& c) {
+    swap(a, b);
+    swap(b, c);
+}
+
+int main() {
+    // Exemples d'utilisation avec différents types:
+
+    // Avec des entiers (int)
+    int x = 10, y = 20, z = 30;
+    cout << "Avant permutation (int): x=" << x << ", y=" << y << ", z=" << z << endl;
+    permutationCirculaireDroite(x, y, z);
+    cout << "Apres permutation (int): x=" << x << ", y=" << y << ", z=" << z << endl;
+
+    // Avec des nombres à virgule flottante (double)
+    double d1 = 1.1, d2 = 2.2, d3 = 3.3;
+    cout << "Avant permutation (double): d1=" << d1 << ", d2=" << d2 << ", d3=" << d3 << endl;
+    permutationCirculaireDroite(d1, d2, d3);
+    cout << "Apres permutation (double): d1=" << d1 << ", d2=" << d2 << ", d3=" << d3 << endl;
+
+    // Avec des caractères (char)
+    char c1 = 'A', c2 = 'B', c3 = 'C';
+    cout << "Avant permutation (char): c1=" << c1 << ", c2=" << c2 << ", c3=" << c3 << endl;
+    permutationCirculaireDroite(c1, c2, c3);
+    cout << "Apres permutation (char): c1=" << c1 << ", c2=" << c2 << ", c3=" << c3 << endl;
+
+    // Avec des chaînes de caractères (string)
+    string s1 = "un", s2 = "deux", s3 = "trois";
+    cout << "Avant permutation (string): s1=" << s1 << ", s2=" << s2 << ", s3=" << s3 << endl;
+    permutationCirculaireDroite(s1, s2, s3);
+    cout << "Apres permutation (string): s1=" << s1 << ", s2=" << s2 << ", s3=" << s3 << endl;
+
+    return 0;
+}
+
+
+/*
+* #include <iostream>
 using namespace std;
 
 // fonction template pour la permutation circulaire droite
@@ -39,3 +83,4 @@ int main() {
 
 
 }
+ */
