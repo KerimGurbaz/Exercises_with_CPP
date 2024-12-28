f543bff714283bc2d6825ea93d83d4a82184bef5
Kerim <kerim.grbz.01@gmail.com>
2024-12-28 11:29:49 +0100

    Fonction de tri par sélection surchargée pour les tableaux de style C

--- triGenerique.cpp
+++ triGenerique.cpp
@@ -28,3 +28,24 @@ void selectionSort(T& container) {
    }
 }
 
+//fonction de tri par sélection surcharge pour les tableaux de style C
+template<typename T2, size_t N>
+void selectionSort(T2(&array)[N]) {
+   for(size_t i = 0; i < N -1 ; ++i) {
+      size_t min_idx = i; // index du plus petit élément à la position actuelle
+
+      //analyser les éléments restant
+      for(size_t j = i+1 ; j<N ; ++j) {
+         if(array[j] < array[min_idx]) {
+            min_idx = j;
+         }
+      }
+      //Echanger si le plus petit element n'est pas dans sa position actuelle
+      if(min_idx != i) {
+         swap(array[i],  array[min_idx]); //Changer les elements
+      }
+   }
+}
+
+
+
