bb2aebd371bbb8e08f7d74b76cb3e0ac5040425d
Kerim <kerim.grbz.01@gmail.com>
2024-12-30 21:51:17 +0100

    Statistique- Calculer et imprimer les totaux

--- statistique.cpp
+++ statistique.cpp
@@ -1,7 +1,81 @@
 //
 // Created by Krm on 30/12/2024.
 //
+
+
 #include <iostream>
+#include <vector>
+using namespace std;
+using Data = double;
+using Ligne = vector<Data>;
+using Matrice = vector<Ligne>;
+
+//fonction generique- pour le calcul total
+template <typename T>
+T calculerSomme(const vector<T>& v) {
+        T somme = 0;
+        for(const T& val : v) {
+                somme += val;
+        }
+        return somme;
+}
+
+
+//fonction générique - pour le calcul de la moyenne
+template<typename T>
+T calculerMoyenne(const vector<T>& v) {
+        if(v.empty()) {
+                return 0;//pour un vecteur vide, la moyenne est accepté comme 0.
+        }
+        return calculerSomme(v) / v.size();
+}
+
+// fonction generique pour imprimer le vecteur
+
+template<typename T>
+void afficherVecteur(const vector<T>& v) {
+        cout<<"[";
+        for(size_t i = 0; i< v.size(); ++i) {
+                cout<<v[i] << (i<v.size() - 1 ? ", " : "");
+
+        }
+        cout<<"]"<<endl;
+}
+
+template<typename T>
+vector<T> calculerSommeOuMoyenne(const vector<vector<T>>& matrice, int operation) {
+        vector<T> resultat;
+        for(const auto& ligne : matrice) {
+                if(operation == 0) {
+                        resultat.push_back(calculerSomme(ligne));
+                }else if(operation ==1) {
+                        resultat.push_back(calculerMoyenne(ligne));
+                }else {
+                        cerr<<"Operation invalide!"<<endl;
+                        return vector<T>(); //returne un vecteur vide en cas d'erreur
+                }
+        }
+
+        return resultat;
+}
+
+int main() {
+        const Matrice notes {{4.0, 5.0, 6.0},  // Jean
+                       {4.1, 5.1, 4.8},  // Marie
+                       {3.5, 4.1     },  // Joshua
+                       {4.5, 4.5, 4.6}}; // Ali
+
+
+
+
+
+
+        return 0;
+}
+
+
+/*
+* #include <iostream>
 #include <vector>
 #include <iomanip>
 using namespace std;
@@ -67,3 +141,4 @@ int main() {
 
         return 0;
 }
+ */
