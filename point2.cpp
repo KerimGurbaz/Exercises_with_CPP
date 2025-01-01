edb44d382851bdb600b9b5383736adcaeba6418a
Kerim <kerim.grbz.01@gmail.com>
2025-01-01 22:37:10 +0100

    Point avec constructeurs v3

--- point2.cpp
+++ point2.cpp
@@ -1,7 +1,91 @@
 //
 // Created by Krm on 01/01/2025.
 //
+
 #include <iostream>
+using namespace std;
+
+class Point {
+private:
+    double x ;
+    double y;
+    double xMax;
+    double yMax;
+
+
+
+    public:
+    Point() : x(0.0), y(0.0),xMax(100.0), yMax(100.0){}
+
+    //Constructeur avec parametre:
+    Point(double x_val, double y_val, double x_Max=100.0, double y_Max= 100.0):x(x_val), y(y_val), xMax(x_Max), yMax(y_Max) {
+        if(x < 0) x =0;
+        if(x > xMax) x = xMax;
+        if(y < 0) y =0;
+        if(y > yMax) y = yMax;
+    }
+
+    void setX(double x_val) {
+        if(x_val >=0 && x_val <=xMax) {
+            x = x_val;
+        }
+    }
+
+    void setY(double y_val) {
+        if(y >=0 && y<=yMax) {
+            y = y_val;
+        }
+    }
+
+    double getX()const {
+        return x;
+    }
+    double getY()const {
+        return y;
+    }
+
+    void afficher()const {
+        cout<<"x = "<<x<<" , y= "<<y<<endl;
+    }
+
+    void deplacer(double dx, double dy) {
+        if(x + dx >=0 && x+dx<=xMax) {
+            x += dx;
+        }if(y + dy >=0 && y+dy<=yMax) {
+            y += dy;
+        }
+
+    }
+};
+
+int main() {
+    Point p1(1.2, 2.4);
+    cout << "p1"; p1.afficher();
+    p1.deplacer(1., 3.);
+    cout << "p1"; p1.afficher();
+    p1.setX(4.);
+    cout << "p1"; p1.afficher();
+    p1.setY(7.);
+    cout << "p1"; p1.afficher();
+
+    Point p2(3., 4.2, 10., 10.);
+    cout << "p2"; p2.afficher();
+    p2.deplacer(15, 2);
+    cout << "p2"; p2.afficher();
+    p2.deplacer(3, 2);
+    cout << "p2"; p2.afficher();
+    p2.setX(-3);
+    cout << "p2"; p2.afficher();
+    p2.setY(12);
+    cout << "p2"; p2.afficher();
+
+    return 0;
+}
+
+
+
+/*
+*#include <iostream>
 using namespace std;
 
 
@@ -58,9 +142,6 @@ int main() {
     return 0;
 }
 
-
-
-/*
  *
 *class Point {
 private:
