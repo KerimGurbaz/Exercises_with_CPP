24c4872763888b5e7282332a9d240fb76062cd82
Kerim <kerim.grbz.01@gmail.com>
2025-01-01 19:46:07 +0100

    Surcharge et spécialisation v2

--- SurchargeEtSpecialisation.cpp
+++ SurchargeEtSpecialisation.cpp
@@ -54,5 +54,39 @@ ostream& operator<<(ostream& os, const array<T, N>& arr) {
     return os;
 }
 
+//fonction sablonu -Transformer
+template<typename T, size_t N, typename Fct>
+void transformer(array<T,N>& arr, Fct fct) {
+    for(size_t i = 0; i < N ; ++i ) {
+        arr[i] = fct(arr[i]);
+    }
+}
+
+int main() {
+
+    cout << opposer(42) << " "
+         << opposer(42u) << " "
+         << boolalpha << opposer(true) << " "
+         << opposer(false) << " "
+         << opposer(-3.14) << endl;
+
+    array<int,0> a0;                                      cout << a0 << endl;
+
+    array<int, 4> a4 { 1, 2, 3,  4 };                     cout << a4 << endl;
+    transformer(a4, opposer<int>);                        cout << a4 << endl;
+
+    array<bool,0> b0;                                     cout << b0 << endl;
+
+    array<bool, 5> b5 { true, false, false, true, true }; cout << b5 << endl;
+    transformer(b5, opposer<bool>);                       cout << b5 << endl;
+
+    array<double,0> c0;                                   cout << c0 << endl;
+
+    array<double, 3> c3 { 1.61, 2.72, 3.14 };             cout << c3 << endl;
+    transformer(c3, opposer<double>);                     cout << c3 << endl;
+
+
+    return 0;
+}
 
 
