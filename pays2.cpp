f53839fdad81391b8b67cfb4d631fc5cace5fb95
Kerim <kerim.grbz.01@gmail.com>
2025-01-02 10:56:44 +0100

    Création d'une liste de pays et Initialisation des variables pour les résultats

--- pays2.cpp
+++ pays2.cpp
@@ -13,6 +13,73 @@ private:
     double population;
     double superficie;
 
+public :
+    Pays(string nom_pays, double pop, double sup): nom(nom_pays), population(pop), superficie(sup){}
+
+    string getNom() const {
+    return nom;
+}
+    double getPopulation() const {
+    return population;
+}
+    double setSuperficie() const {
+    return superficie;
+}
+
+    //Methode pour calculer la densite de population(habitants par km)
+    double densite()const {
+    if(superficie == 0) {
+        return 0;// eviter la divition pas zero
+    }
+    return (population * 1000000)/ superficie;
+}
+};
+
+
+int main() {
+    // creation d'une liste de pays
+    vector<Pays> p;
+    p.emplace_back("Suisse", 8.121830, 41290);
+    p.emplace_back("France", 66.663766, 547030);
+    p.emplace_back("Italie", 61.855120, 301230);
+    p.emplace_back("Allemagne", 80.854408, 357021);
+
+
+    string plus_grande_superficie ="";
+    string plus_peuple = "";
+    string plus_grande_densite ="";
+
+    double max_superficie = -1.0;
+    double max_population = -1.0;
+    double max_densite = -1.0;
+
+    //Parcourir la liste des pays pour trouver les résultats
+    for(const auto& pays : p) {
+
+    }
+
+
+
+
+
+
+
+    return 0;
+
+}
+
+
+
+
+
+
+/*
+* class Pays {
+private:
+    string nom;
+    double population;
+    double superficie;
+
 public:
     Pays(string nom_pays, double population_pays, double superficie_pays):nom(nom_pays), population(population_pays), superficie(superficie_pays){}
 
@@ -63,3 +130,4 @@ int main() {
 
     return 0;
 }
+ */
