fdf2e8482db5231797d46e4c9e27fbfe2cbe5e6a
Kerim <kerim.grbz.01@gmail.com>
2025-01-02 10:32:53 +0100

    Définition de la classe Pays ,constructeur et getters v1

--- pays2.cpp
+++ pays2.cpp
@@ -32,3 +32,34 @@ public:
         return population *1000000/ superficie;
     }
 };
+
+int main() {
+    vector<Pays> pays = {
+        Pays("Suisse", 8.121830, 41290),
+         Pays("France", 66.663766, 547030),
+         Pays("Italie", 61.855120, 301230),
+         Pays("Allemagne", 80.854408, 357021)
+
+    };
+
+    Pays* paysPlusGrandeSuperficie = &pays[0];
+    Pays* paysPlusPeuple = &pays[0];
+    Pays* paysPlusGrandeDensite = &pays[0];
+
+    for(size_t i = 1; i<pays.size(); ++i) {
+        if(pays[i].getSuperficie() > paysPlusGrandeSuperficie -> getSuperficie()) {
+            paysPlusGrandeSuperficie = &pays[i];
+        }
+        if(pays[i].getPopulation() > paysPlusPeuple -> getPopulation()) {
+            paysPlusPeuple = &pays[i];
+        }
+        if(pays[i].getDensite() > paysPlusGrandeDensite -> getDensite()) {
+            paysPlusGrandeDensite = &pays[i];
+        }
+    }
+    cout << "Pays ayant la plus grande superficie               : " << paysPlusGrandeSuperficie->getNom() << endl;
+    cout << "Pays ayant le plus d'habitants                     : " << paysPlusPeuple->getNom() << endl;
+    cout << "Pays ayant la densite de population la plus grande : " << paysPlusGrandeDensite->getNom() << endl;
+
+    return 0;
+}
