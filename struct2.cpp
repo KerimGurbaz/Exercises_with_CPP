18c0743587ca1479c92aff8de184a6648d8e1f16
Kerim <kerim.grbz.01@gmail.com>
2025-03-07 23:05:40 +0100

    struct vs..

---
+++ struct2.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Krm on 07.03.2025.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << std::endl;
+
+    struct Coord {
+        int x, y ,z;
+    };
+
+    struct Cercle {
+        int rayon;
+        Coord centre;
+    };
+
+    const Coord origine = {0, 0, 0};
+    Cercle cercle = {1, {2,3,4}};
+    Cercle *ptr = &cercle;
+    cout << (*(&cercle)).rayon<<endl;;
+
+
+
+    cout<<cercle.rayon<<endl;
+    cout<<cercle .rayon;
+
+    return 0;
+}
