f74c191b904cc6d72b0ef3fad0d06afab2497ad8
Kerim <kerim.grbz.01@gmail.com>
2025-04-11 23:41:42 +0200

    à partir d’une chaîne de caractères et de deux autres chaînes représentant respectivement les caractères source et leur correspondance, remplace chaque occurrence (en ignorant la casse) par son équivalent -v2

--- chaines_de_caracters.cpp
+++ chaines_de_caracters.cpp
@@ -1,14 +1,9 @@
 //
 // Created by Krm on 09.04.2025.
 //
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <cctype>
 
-using namespace std;
-
-string toLower(string s) {
+/*
+* string toLower(string s) {
     transform(s.begin(), s.end(), s.begin(), ::tolower);
     return s;
 }
@@ -48,6 +43,48 @@ int main() {
     int nombre_remplacements = transformer(texte, source, correspondance);
     cout << "Texte après transformation : " << texte << endl; // Résultat attendu : HXXlo World!
     cout << "Nombre de remplacements : " << nombre_remplacements << endl; // Résultat attendu : 2
+
+    return 0;
+}
+ */
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+int transformer(string & texte, const string& source, const string &cible) {
+    if(source.length() != cible.length()) {
+        return 0;
+    }
+
+    int remplacements = 0;
+
+    for(char &c : texte) {
+        char c_petit = tolower(c);
+
+        for(size_t i = 0; i<source.length(); ++i) {
+        char s_petit = tolower(source[i]);
+        if(c_petit == s_petit) {
+            c = cible[i];
+            remplacements ++;
+            break;
+        }
+    }
+    }
+
+return remplacements;
+}
+int main() {
+
+    string texte2 = "Data Engineering";
+    string source2 = "aeiou";
+    string correspondance2 = "12345";
+    int nombre_remplacements2 = transformer(texte2, source2, correspondance2);
+    cout << "Texte après transformation : " << texte2 << endl; // Résultat attendu : D1t1 Eng3n22r3ng
+    cout << "Nombre de remplacements : " << nombre_remplacements2 << endl; // Résultat attendu : 6
+
 
     return 0;
 }
