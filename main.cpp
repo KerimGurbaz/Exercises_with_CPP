06ab2d6496ebc8db57f968873c22bd51af5293d2
Kerim <kerim.grbz.01@gmail.com>
2025-06-28 00:49:19 +0200

    Tri générique

--- main.cpp
+++ main.cpp
@@ -171,8 +171,6 @@ cout<<"apres"<<endl;
         cout<<val<<" ";   }
     return 0;
 }
-
- */
 template<typename T>
 T& select(T& a, T&b, bool select_a) {
     return select_a ? a : b;
@@ -194,4 +192,101 @@ int main() {
 
     return 0;
 }
+template<typename T>
+T somme(T a, T b) {
+    return (a+b);
+}
 
+int main() {
+    int   a = 1;
+    float b = 2.5f;
+
+    cout<<somme(static_cast<float>(a),b)<<" / "<<somme(b,b);
+
+    return 0;
+}
+template <typename Container>
+void printContainer(const Container & container) {
+    cout<<"[";
+    for(auto it = begin(container); it != end(container); ++it) {
+        cout<<*it;
+        if(next(it) != end(container)) {
+            cout<<",";
+        }
+    }
+    cout<<"]";
+}
+ */
+#include <array>
+
+template<typename T>
+void print_container(const T& container) {
+    cout<<"[";
+    for(auto it = begin(container) ; it != end(container); ++it) {
+        cout<<*it;
+        if(next(it) != end(container)) {
+            cout<<", ";
+        }
+    }
+    cout<<"]";
+}
+
+
+
+
+
+template<typename T>
+void selection_sort(T begin, T end) {
+    for(T it = begin ; it != end; ++it) {
+        T min_it = it;
+
+        for(T j = next(it); j != end; ++j) {
+            if(*j < *min_it) {
+                min_it =j;
+            }
+        }
+        iter_swap(it, min_it);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+    // --- Initialisation des conteneurs ---
+    vector<int> v = {6, 2, 8, 7, 1, 3};
+    array<string, 4> a = {"chien"s, "chat"s, "souris"s, "poisson"s};
+    double t[] = {6.1, 2.2, 8.3, 7.4, 1.5, 3.6};
+
+    // --- Traitement du vector<int> ---
+    cout << "Vector (avant tri) : ";
+    print_container(v);
+    selection_sort(begin(v), end(v));
+    cout << "Vector (après tri) : ";
+    print_container(v);
+    cout << endl;
+
+    // --- Traitement du array<string, 4> ---
+    cout << "Array (avant tri) : ";
+    print_container(a);
+    selection_sort(begin(a), end(a));
+    cout << "Array (après tri) : ";
+    print_container(a);
+    cout << endl;
+
+    // --- Traitement du C-style array double[] ---
+    cout << "C-style Array (avant tri) : ";
+    print_container(t);
+    // std::begin et std::end fonctionnent aussi pour les tableaux C-style.
+    selection_sort(begin(t), end(t));
+    cout << "C-style Array (après tri) : ";
+    print_container(t);
+
+    return 0;
+}
